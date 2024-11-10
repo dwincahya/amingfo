3d7c41c63da49ac0de7ed32f13fd51bca81e9483
dwincahya <dwincahya8@gmail.com>
2024-11-10 16:38:38 +0700

    amingfo

--- src/app/populer/page.jsx
+++ src/app/populer/page.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+"use client";
+import AnimeList from "@/components/AnimeList";
+import HeaderMenu from "@/components/Utilities/HeaderMenu";
+import Pagination from "@/components/Utilities/Pagination";
+import React from "react";
+import { useEffect, useState } from "react/cjs/react.production.min";
 
 const Page = () => {
-  return (
-    <div>page</div>
-  )
-}
+  const [page, setPage] = useState(1);
+  const [topAnime, setTopAnime] = useState([]);
 
-export default Page
+  const fetchData = async () => {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime?page=${page}`
+    );
+    const data = await response.json();
+    setTopAnime(data);
+  };
+
+  useEffect(() =>{
+    fetchData()
+  }, [page])
+
+  return (
+    <>
+      <HeaderMenu />
+      <AnimeList api={topAnime}/>
+      <Pagination />
+    </>
+  );
+};
+
+export default Page;
--- src/app/search/[keyword]/page.jsx
+++ src/app/search/[keyword]/page.jsx
@@ -1,19 +1,18 @@
 import Header from "@/components/AnimeList/Header";
 import AnimeList from "@/components/AnimeList";
 
-const Page = async ({params}) => {
-    const { keyword} = params
+const Page = async ({ params }) => {
+  const { keyword } = params;
+  const decodekeyword = decodeURI(keyword);
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_BASE_URL}/anime?q=${keyword}`
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/anime?q=${decodekeyword}`
   );
   const searchAnime = await response.json();
 
   return (
     <>
       <section>
-        <Header
-          title={`Pencarian Untuk ${keyword}`}
-        />
+        <Header title={`Pencarian Untuk ${decodekeyword}`} />
         <AnimeList api={searchAnime} />
       </section>
     </>
