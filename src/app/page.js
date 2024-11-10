9b83acb14b27b298369ddc081a104e12ff29959c
dwincahya <dwincahya8@gmail.com>
2024-11-10 16:08:53 +0700

    amingfo

--- src/app/page.js
+++ src/app/page.js
@@ -1,34 +1,24 @@
-import Link from "next/link";
+import Header from "@/components/AnimeList/Header";
 import AnimeList from "@/components/AnimeList";
 
-
-const Home = async () => {
+const Page = async () => {
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime?limit=8`
   );
-  const anime = await response.json();
+  const topAnime = await response.json();
 
   return (
-    <div>
-      <div className="p-4 flex justify-between items-center">
-        <h1 className="text-2xl font-bold">Paling Populer</h1>
-        <Link href="/populer" className="md:text-xl text-sm underline font-semibold hover:text-blue-800 transition-all">Lihat Semua</Link>
-      </div>
-      <div className="grid px-4 md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4">
-        {anime.data.map((data) => {
-          return (
-            <div key={data.mal_id} className="shadow-xl">
-              <AnimeList
-                title={data.title}
-                images={data.images.webp.image_url}
-                id={data.mal_id}
-              />
-            </div>
-          );
-        })}
-      </div>
-    </div>
+    <>
+      <section>
+        <Header
+          title="Paling Populer"
+          linkTitle="Lihat Semua"
+          linkHref="/populer"
+        />
+        <AnimeList api={topAnime} />
+      </section>
+    </>
   );
 };
 
-export default Home;
+export default Page;
--- src/components/Utilities/Navbar/index.js
+++ src/components/Utilities/Navbar/index.js
@@ -1,11 +1,12 @@
 import Link from "next/link"
+import InputSearch from "./InputSearch"
 
 const Navbar = () => {
     return (
-        <header className="bg-blue-800">
-        <div className="flex md:flex-row flex-col gap-2 justify-between p-4">
+        <header className="bg-color-accent">
+        <div className="flex md:flex-row flex-col gap-2 md:items-center justify-between p-4">
             <Link href="/" className="font-bold text-2xl text-white">Amingfo</Link>
-            <input placeholder="Cari Anime" className="" />
+            <InputSearch />
         </div>
         </header>
     )
