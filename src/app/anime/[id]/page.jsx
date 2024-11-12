c459c1e0bb75db3d4d6ea6ccaec0a74c0000d447
dwincahya <dwincahya8@gmail.com>
2024-11-12 22:33:35 +0700

    amingfo

--- src/app/anime/[id]/page.jsx
+++ src/app/anime/[id]/page.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import { getAnimeRespons } from "@/app/service/api-service";
+import Image from "next/image";
+import React from "react";
 
-const Page = () => {
+const Page = async ({ params: { id } }) => {
+  const anime = await getAnimeRespons(`anime/${id}`);
+
   return (
-    <div>Page</div>
-  )
-}
+    <>
+      <div className="pt-4 px-4">
+        <h3 className="text-2xl text-color-primary">
+          {anime.data.title} - {anime.data.year}
+        </h3>
+      </div>
+      <div className="pt-4 px-4 flex sm:flex-nowrap flex-wrap gap-2 text-color-primary">
+        <Image
+          src={anime.data.images.webp.image_url}
+          alt={anime.data.images.jpg.image_url}
+          width={250}
+          height={250}
+          className="w-full rounded object-cover"
+        />
+        <p className="text-justify text-xl">{anime.data.synopsis}</p>
+      </div>
+    </>
+  );
+};
 
-export default Page
+export default Page;
