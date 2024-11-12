e42933c8e303cfaede0273de433517447549181b
dwincahya <dwincahya8@gmail.com>
2024-11-12 20:05:05 +0700

    amingfo

--- src/components/Utilities/Footer/index.js
+++ src/components/Utilities/Footer/index.js
@@ -16,10 +16,10 @@ const Footer = () => {
                   <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Resources</h2>
                   <ul className="text-gray-500 dark:text-gray-400 font-medium">
                       <li className="mb-4">
-                          <a href="https://flowbite.com/" className="hover:underline">Flowbite</a>
+                          <a href="https://flowbite.com/" className="hover:underline">Amingfo</a>
                       </li>
                       <li>
-                          <a href="https://tailwindcss.com/" className="hover:underline">Tailwind CSS</a>
+                          <a href="https://tailwindcss.com/" className="hover:underline">Anime Populer</a>
                       </li>
                   </ul>
               </div>
