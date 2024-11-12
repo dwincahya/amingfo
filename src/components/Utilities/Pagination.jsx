107a9af8aa76b2a906ab31caa15f2b25f18d763d
dwincahya <dwincahya8@gmail.com>
2024-11-12 21:53:28 +0700

    amingfo

--- src/components/Utilities/Pagination.jsx
+++ src/components/Utilities/Pagination.jsx
@@ -1,3 +1,10 @@
+import {
+  FaAngleLeft,
+  FaAngleRight,
+  FaAngleDoubleRight,
+  FaAngleDoubleLeft,
+} from "react-icons/fa";
+
 const Pagination = ({ page, lastPage, setPage }) => {
   const scrollTop = () => {
     scrollTo({
@@ -16,28 +23,56 @@ const Pagination = ({ page, lastPage, setPage }) => {
     scrollTop();
   };
 
+  const handleLastPage = () => {
+    setPage(lastPage);
+    scrollTop();
+  };
+
+  const handleFirstPage = () => {
+    setPage(1);
+    scrollTop();
+  };
+
   return (
     <div className="flex justify-center items-center py-4 px-2 gap-4 text-color-primary text-2xl">
-      {page <= 1 ? null : (
-        <button
-          onClick={handlePrevPage}
-          className="transition-all hover:text-color-accent"
-        >
-          Prev
-        </button>
+      {page > 1 && (
+        <>
+          <button
+            onClick={handleFirstPage}
+            className="transition-all hover:text-color-accent"
+          >
+            <FaAngleDoubleLeft />
+          </button>
+
+          <button
+            onClick={handlePrevPage}
+            className="transition-all hover:text-color-accent"
+          >
+            <FaAngleLeft />
+          </button>
+        </>
       )}
 
       <p>
         {page} of {lastPage}
       </p>
 
-      {page >= lastPage ? null : (
-        <button
-          onClick={handleNextPage}
-          className="transition-all hover:text-color-accent"
-        >
-          Next
-        </button>
+      {page < lastPage && (
+        <>
+          <button
+            onClick={handleNextPage}
+            className="transition-all hover:text-color-accent"
+          >
+            <FaAngleRight />
+          </button>
+
+          <button
+            onClick={handleLastPage}
+            className="transition-all hover:text-color-accent"
+          >
+            <FaAngleDoubleRight />
+          </button>
+        </>
       )}
     </div>
   );
