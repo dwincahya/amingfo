daadb6cf58fc198de59aeb53d76e23eb51773b1a
dwincahya <dwincahya8@gmail.com>
2024-11-14 19:47:19 +0700

    amingfo

--- src/components/Utilities/Navbar/UserButton.jsx
+++ src/components/Utilities/Navbar/UserButton.jsx
@@ -1,12 +1,10 @@
-import { authOption } from '@/app/api/auth/[...nextauth]/route'
-import { getServerSession } from 'next-auth'
+import { authUserSession } from '@/service/auth-service'
 import Link from 'next/link'
 import React from 'react'
 
 const UserButton = async() => {
-    const user = await getServerSession(authOption)
-    console.log(user)
-
+    const user = await authUserSession();
+    
   return (
     <div><Link href="/api/auth/signin" className="">Sign In</Link></div>
   )
