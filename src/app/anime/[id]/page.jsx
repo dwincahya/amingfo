969027ee64e9675ad6e100fbd02a915d321be8d7
dwincahya <dwincahya8@gmail.com>
2024-11-19 22:02:47 +0700

    amingfo

--- src/app/anime/[id]/page.jsx
+++ src/app/anime/[id]/page.jsx
@@ -35,7 +35,7 @@ const Page = async ({ params: { id } }) => {
           <p>{anime.data.favorites}</p>
         </div>
       </div>
-      <div className="pt-4 px-4 flex sm:flex-nowrap flex-wrap gap-2 text-color-primary">
+      <div className="pt-8 px-6 flex sm:flex-nowrap flex-wrap gap-4 text-color-primary">
         <Image
           src={anime.data.images.webp.image_url}
           alt={anime.data.images.jpg.image_url}
